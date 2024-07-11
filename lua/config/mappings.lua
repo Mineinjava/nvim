4d5fccbe3efc0ecdff430c3007cee5487ea69c47
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2024-07-11 16:03:32 -0700

    add copy/paste

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,3 +1,5 @@
+LEADER = ","
+
 function map(mode, shortcut, command)
   vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
 end
@@ -10,5 +12,16 @@ function imap(shortcut, command)
   map('i', shortcut, command)
 end
 
+function vmap(shortcut, command)
+  map('v', shortcut, command)
+end
+
 nmap("<", ":call cursor(line(\".\"), 72)\n")
+
+vmap(string.format("%sy", LEADER), '"+y')
+nmap(string.format("%sY", LEADER), '"+yg_')
+nmap(string.format("%sy", LEADER), '"+y')
+
+nmap(string.format("%sP", LEADER), '"+P')
+nmap(string.format("%sp", LEADER), '"+p')
 
